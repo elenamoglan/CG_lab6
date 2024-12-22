36122a6a6347db85f3e52965c3ea37043b30cecd
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2024-12-23 01:35:56 +0200

    Update untitled.m

--- untitled.m
+++ untitled.m
@@ -8,9 +8,11 @@ points = [3, -5;    % A
 % Compute Delaunay Triangulation
 tri = delaunay(points(:,1), points(:,2));
 
+% Create a larger figure
+figure('Position', [100, 100, 1200, 600]); % Adjust size as needed
+
 % Plot Delaunay Triangulation
-figure;
-subplot(1, 2, 1);
+subplot(1, 2, 1, 'Position', [0.05, 0.1, 0.4, 0.8]); % [left, bottom, width, height]
 triplot(tri, points(:,1), points(:,2), 'g');
 hold on;
 scatter(points(:,1), points(:,2), 'ro', 'filled');
@@ -19,13 +21,14 @@ for i = 1:length(points)
     text(points(i,1), points(i,2), labels{i}, 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
 end
 title('Delaunay Triangulation');
+grid;
 hold off;
 
 % Compute Voronoi Diagram
 [vx, vy] = voronoi(points(:,1), points(:,2));
 
 % Plot Voronoi Diagram
-subplot(1, 2, 2);
+subplot(1, 2, 2, 'Position', [0.55, 0.1, 0.4, 0.8]); % [left, bottom, width, height]
 plot(vx, vy, 'b');
 hold on;
 scatter(points(:,1), points(:,2), 'ro', 'filled');
@@ -33,4 +36,5 @@ for i = 1:length(points)
     text(points(i,1), points(i,2), labels{i}, 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
 end
 title('Voronoi Diagram');
+grid;
 hold off;
